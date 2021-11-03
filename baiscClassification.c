4f672a8dd57e6ef50f46a951c4d0be9153515cd4
zeevfischer <92921822+zeevfischer@users.noreply.github.com>
2021-11-03 10:56:04 +0200

    Update baiscClassification.c

--- baiscClassification.c
+++ baiscClassification.c
@@ -3,11 +3,11 @@
 
 int isPrime(int a)
 {
-    if(a==0 || (a>2 && a%2==0))
+    if(a==0 || a==1)
     {
         return 0;
     }
-    for(int i = 1 ;i < a/2;i++)
+    for(int i = 2 ;i < a;i++)
     {
         if(a % i == 0)
         {
